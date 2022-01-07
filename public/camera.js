5a169d1eb11cb692abb4aca4a76fa0a361bc2695
wakaba0972 <benny.wu2199@gmail.com>
2022-01-08 00:59:18 +0800

    creat camera.js

---
+++ public/camera.js
@@ -0,0 +1,37 @@
+var display = document.getElementById('display')
+var canvas = document.createElement('canvas')
+var btn = document.getElementById('savBtn')
+var context = canvas.getContext('2d');
+
+
+function openCam(){
+    let constraints = {video: true}
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            display.srcObject = stream
+            display.play()
+        })
+        .catch(function (error) {
+            btn.innerText = error
+        });
+}
+
+function getURL() {
+    return new Promise(function(resolve, reject){
+        canvas.width = display.videoWidth
+        canvas.height = display.videoHeight
+        context.drawImage(display, 0, 0, display.videoWidth, display.videoHeight);
+        const dataUri = canvas.toDataURL('image/jpg');
+
+        axios.post('/detect', {
+            data: dataUri.split(',')[1]
+        })
+        .then(res=> {
+            console.log(res.data)
+            resolve(window.location.href + '/' + res.data)
+        })
+        .catch(err=> {
+            reject(err)
+        })
+    })
+}
