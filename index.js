4295f422d90e1b75b4dd09b5017d1c5928689671
wakaba0972 <benny.wu2199@gmail.com>
2022-01-08 19:57:30 +0800

    delete persondata

--- index.js
+++ index.js
@@ -40,20 +40,9 @@ function save(data){
     })
 }
 
-function saveUserData(name, personId){
-    return new Promise(function(resolve, reject){
-        let data = JSON.stringify({name: name});
-        fs.writeFile('./public/PersonData/' + personId + '.json', data, function(err){
-            if(err) reject(err)
-            else resolve(personId)
-        });
-    })
-}
-
 function create(name, urls){
     return new Promise(function(resolve, reject){
         faceapi.createPerson(name)
-        .then(personId=> saveUserData(personId))
         .then(personId=> {
             let num = 0;
             for(let i=0; i<3; ++i){
