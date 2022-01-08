80b9b0f346229b99bda17c09b0017a2a2564fbc4
wakaba0972 <benny.wu2199@gmail.com>
2022-01-08 17:39:23 +0800

    sd

--- ss.js
+++ ss.js
@@ -3,12 +3,10 @@ var fs = require('fs')
 
 function saveUserData(name, personId){
     return new Promise(function(resolve, reject){
-        let data = JSON.stringify({name: name});
-        fs.writeFile('./Persons Data/' + personId + '.json', data, function(err){
-            if(err) reject(err)
-            else resolve(personId)
-        });
+        fs.readFile('./PersonsData.json')
+        raw.personId = name
+        fs.writeFileSync('./PersonsData.json', raw)
+        resolve(personId)
     })
 }
-
 saveUserData('asd', 'sd').then(a=> console.log(a))
