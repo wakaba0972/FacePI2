ef05b288c6c29271b88327b345da7b11857843bd
wakaba0972 <benny.wu2199@gmail.com>
2022-01-08 15:15:40 +0800

    asdasd

--- self_modules/faceAPI.js
+++ self_modules/faceAPI.js
@@ -17,7 +17,7 @@ module.exports.detect = function(url) {
                 resolve({msg: 'wrong'})
             }
             else {
-                console.log(JSON.parse(res.data) == '[]')
+                console.log(JSON.stringify(res.data) == '[]')
                 if(res.data[0].faceId){
                     res.data[0].url = url
                     console.log(res.data[0])
