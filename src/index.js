7063460342c480f1daeede63b82b398c81f5dd61
wakaba0972 <benny.wu2199@gmail.com>
2022-02-01 23:13:39 +0800

    debug

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ var app = express()
     })
     .get('/chatRoom', (req, res)=> {
         let key = req.params.key
+        console.log(key)
         let now = Date.now()
         Object.keys(KEYS).forEach((key)=> {
             if(now - KEYS[key].start > 300000) delete KEYS[key]
